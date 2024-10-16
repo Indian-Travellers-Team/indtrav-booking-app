b9b714d6cc15869823a59e8935f2a478a90a45f7
deepakmehta1 <deepak.deep40@yahoo.com>
2024-10-17 02:19:37 +0530

    integration the get packages API

---
+++ src/api/packageService.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = process.env.REACT_APP_API_URL + '/packages/api/packages/';
+
+export const fetchPackages = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data; // Return the response data directly
+  } catch (error) {
+    console.error("Error fetching packages:", error);
+    throw error; // Rethrow the error to handle it later in the calling function
+  }
+};
