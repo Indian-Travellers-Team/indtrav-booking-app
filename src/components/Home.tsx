79e0accb588b74d00690d255f4bb52c80472aa30
deepakmehta1 <deepak.deep40@yahoo.com>
2024-10-16 22:17:33 +0530

    enhacement in the css for home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Home.css'; 
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Home: React.FC = () => {
   return (
     <div className="home-container">
-      <h1 className="title">
-        <i className="fa fa-calendar-day"></i> {/* Use 'fas' for solid icons */}
-        Booking Options
-      </h1>
-      <p className="subtitle">Choose the type of booking that suits your needs.</p>
-      <div className="button-container">
-        <Link to="/booking/single">
-          <div className="button single-booking">
-            <span>👤 Single Person Booking</span>
+      <div className="content-container"> {/* New container for semi-transparent background */}
+        <h1 className="title">
+          <i className="fas fa-calendar-day"></i> Booking Options
+        </h1>
+        <p className="subtitle">Choose the type of booking that suits your needs.</p>
+        <div className="button-container">
+          <div className="button-wrapper">
+            <Link to="/booking/single">
+              <div className="button single-booking">
+                <span>👤 Single Person Booking</span>
+              </div>
+            </Link>
             <p className="description">Select this option if you're booking for yourself.</p>
           </div>
-        </Link>
-        <Link to="/booking/multiple">
-          <div className="button multiple-booking">
-            <span>👥 Multiple Person Booking</span>
+
+          <div className="button-wrapper">
+            <Link to="/booking/multiple">
+              <div className="button multiple-booking">
+                <span>👥 Multiple Person Booking</span>
+              </div>
+            </Link>
             <p className="description">Choose this for booking on behalf of a group or multiple people.</p>
           </div>
-        </Link>
+        </div>
       </div>
     </div>
   );
