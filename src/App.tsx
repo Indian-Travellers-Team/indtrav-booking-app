e83295ed782138e838d5390611d179c1b4cbb326
deepakmehta1 <deepak.deep40@yahoo.com>
2024-10-18 16:36:32 +0530

    enforce logging and add logout button on the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import BookingForm from './components/BookingForm';
-import Login from './components/Login'; // Import the Login component
+import Login from './components/Login';
+import { AuthProvider, useAuth } from './AuthContext';
+
+const ProtectedRoute: React.FC<{ element: JSX.Element }> = ({ element }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) return <div>Loading...</div>; // Optionally show a loading state
+
+  return user ? element : <Navigate to="/login" />;
+};
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/bookings" element={<Home />} />
-        <Route path="/booking/:type" element={<BookingForm />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/bookings"
+            element={<ProtectedRoute element={<Home />} />}
+          />
+          <Route
+            path="/booking/:type"
+            element={<ProtectedRoute element={<BookingForm />} />}
+          />
+          {/* Add more protected routes as needed */}
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthProvider>
   );
 };
 
